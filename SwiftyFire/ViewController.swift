f1d9a68e57dfb4e3497d31b67f66df562b124e2c
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-07-05 04:38:58 +0600

    init commit

---
+++ SwiftyFire/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftyFire
+//
+//  Created by Md Khaled Hasan Manna on 7/5/19.
+//  Copyright © 2019 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnClickAction(_ sender: Any) {
+    }
+    
+}
+
